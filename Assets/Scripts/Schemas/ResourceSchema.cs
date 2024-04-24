6a2a33e4d0e7d29190f8b70a0ca1e1d18875d9b6
ZotChryis <navac@outlook.com>
2024-04-23 22:09:57 -0700

    Unrefined Resources, Update UI, and Extractor generic building

--- Assets/Scripts/Schemas/ResourceSchema.cs
+++ Assets/Scripts/Schemas/ResourceSchema.cs
@@ -9,7 +9,39 @@ namespace Schemas
     [CreateAssetMenu]
     public class ResourceSchema : Schema
     {
+        public enum ResourceType
+        {
+            // Unrefined
+            Amber = 0,
+            Dino = 1,
+            Lava = 2,
+            Fossil = 3,
+            
+            // Refined
+            Might = 4,
+            Prosperity = 5,
+            Culture = 6,
+            Technology = 7,
+        } 
+        
         public string Name;
         public Sprite Icon;
+        public ResourceType Type;
+
+        public bool IsRefined()
+        {
+            return Type == ResourceType.Might ||
+                   Type == ResourceType.Prosperity ||
+                   Type == ResourceType.Culture ||
+                   Type == ResourceType.Technology;
+        }
+        
+        public bool IsUnrefined()
+        {
+            return Type == ResourceType.Amber ||
+                   Type == ResourceType.Dino ||
+                   Type == ResourceType.Lava ||
+                   Type == ResourceType.Fossil;
+        }
     }
 }
--- Assets/Scripts/UI/BankView.cs
+++ Assets/Scripts/UI/BankView.cs
@@ -5,14 +5,15 @@ namespace UI
     public class BankView : MonoBehaviour
     {
         [SerializeField] private ResourceView m_prefab;
-        [SerializeField] private Transform m_content;
+        [SerializeField] private Transform m_refined;
+        [SerializeField] private Transform m_unrefined;
 
         public void Start()
         {
             // TEMP: Show that we can pull the data to create the views
             foreach (var resourceSchema in ServiceLocator.Instance.Schemas.Resources)
             {
-                ResourceView resource = Instantiate(m_prefab, m_content);
+                ResourceView resource = Instantiate(m_prefab, resourceSchema.IsRefined() ? m_refined : m_unrefined);
                 resource.SetData(resourceSchema);
             }
         }
