ec865aa466bb3e6f09b0eb5bd896d3e01b289e53
dsamia25 <42752755+dsamia25@users.noreply.github.com>
2024-05-20 21:12:15 -0700

    Added TooltipDescription class
    - TooltipDescription class wraps tooltip title, description, image, actions, and anything else that could be needed.

--- Assets/Scripts/UI/TooltipView.cs
+++ Assets/Scripts/UI/TooltipView.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
 using Schemas.Actions;
 
@@ -8,14 +9,55 @@ namespace UI
 {
     public class TooltipView : View
     {
-        [SerializeField] private TextMeshProUGUI m_titleText;
-        [SerializeField] private TextMeshProUGUI m_messageText;
-        //[SerializeField] private 
-
-        public void Activate(string title, string message)
+        /// <summary>
+        /// Holds all information that would be displayed in a tooltip.
+        /// </summary>
+        public class TooltipDescription
         {
-            m_titleText.text = title;
-            m_messageText.text = message;
+            public string title;
+            public string description;
+            public Sprite sprite;
+            // TODO: Add action list
+        }
+
+        [SerializeField] private TextMeshProUGUI m_titleText;
+        [SerializeField] private TextMeshProUGUI m_descriptionText;
+        [SerializeField] private Image m_image;
+
+        public delegate void TooltipButtonDelegate();
+
+        public void SetData(TooltipDescription tooltip)
+        {
+            // Set title text
+            if (m_titleText != null && tooltip.title != null)
+            {
+                m_titleText.text = tooltip.title;
+            } else
+            {
+                m_titleText.gameObject.SetActive(false);
+            }
+
+            // Set description/message text
+            if (m_descriptionText != null && tooltip.description != null)
+            {
+                m_descriptionText.text = tooltip.description;
+            }
+            else
+            {
+                m_descriptionText.gameObject.SetActive(false);
+            }
+
+            // Set image
+            if (m_image != null && tooltip.sprite != null)
+            {
+                m_image.sprite = tooltip.sprite;
+            }
+            else
+            {
+                m_image.gameObject.SetActive(false);
+            }
+
+            // TODO: Create action buttons
         }
     }
 }
